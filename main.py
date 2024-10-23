297cc807262500bc869e26e60bf1990b7528f489
Ell-716 <elenabai.2021@gmail.com>
2024-10-23 16:35:07 +0200

    Create add_tasks function

---
+++ main.py
@@ -0,0 +1,37 @@
+MENU = """
+*****TASKS MENU*****
+--------------------
+1. Add a task
+2. View tasks
+3. Mark a task as complete
+4. Delete a task
+5. Exit the program
+--------------------
+"""
+
+tasks = []
+
+
+def add_tasks():
+
+    while True:
+        try:
+            new_task = input("Please add a task: ").strip()
+            if not new_task:
+                print("Invalid input. Task cannot be empty. Try again!")
+                continue
+
+            tasks.append(new_task)
+            print(f"{new_task} successfully added.")
+
+            while True:
+                another_task = input("Would you like to add another task? y/n: ").strip().lower()
+                if another_task == "y":
+                    break
+                elif another_task == "n":
+                    return
+                else:
+                    print("Invalid choice. Try again!")
+        except ValueError:
+            print("An error occurred. Please try again!")
+
