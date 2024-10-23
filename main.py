a12cc01097a17818d456e36901f45209a0bdf154
Ell-716 <elenabai.2021@gmail.com>
2024-10-23 17:09:00 +0200

    Create mark_task_complete function

--- main.py
+++ main.py
@@ -45,3 +45,18 @@ def view_tasks():
         print("-" * 11)
         for index, task in enumerate(tasks, 1):
             print(f"{index}. {task}")
+
+
+def mark_task_complete():
+    view_tasks()
+
+    try:
+        task_to_complete = int(input("\nWhich task do you want to complete? ")) - 1
+
+        if 0 <= task_to_complete < len(tasks):
+            tasks[task_to_complete] = f"{tasks[task_to_complete]} ✅"
+            print(f"Task {task_to_complete + 1} marked as complete!")
+        else:
+            print("Invalid task number! Please try again.")
+    except ValueError:
+        print("Please enter a valid number.")
